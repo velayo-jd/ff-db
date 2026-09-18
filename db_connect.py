b26b4e34e2304c4daa1b7cf5dea6e7b1015e9709
velayo <velayojohndaniel@gmail.com>
2026-09-18 17:52:28 +0800

    added db_connect, used for connecting to database

---
+++ db_connect.py
@@ -0,0 +1,10 @@
+import mysql.connector as SQLC
+
+def connect():
+    DataBase = SQLC.connect(
+        host = "localhost"
+        user = "root"
+        password = ""
+        database = "ff_db"
+    )
+    return DataBase
