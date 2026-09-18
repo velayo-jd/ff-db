e3081e16ba24bc86967b9ec5cde9325311ebae63
velayo <velayojohndaniel@gmail.com>
2026-09-18 22:11:15 +0800

    solved comma bug

--- db_connect.py
+++ db_connect.py
@@ -2,9 +2,9 @@ import mysql.connector as SQLC
 
 def connect():
     DataBase = SQLC.connect(
-        host = "localhost"
-        user = "root"
-        password = ""
+        host = "localhost",
+        user = "root",
+        password = "",
         database = "ff_db"
     )
     return DataBase
