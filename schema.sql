5fa4a0891df54b7be4d9f53a80c80c597512bb9b
velayo <velayojohndaniel@gmail.com>
2026-09-21 17:29:15 +0800

    added locations

--- schema.sql
+++ schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE terminologies (
     term_type VARCHAR(100),
     term_definition TEXT
 );
+
+CREATE TABLE locations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL, -- Central Nu Regional Hospital
+    type VARCHAR(50), -- Hospital
+    region TEXT, -- Central Nu, near The Medical Recidency
+    status VARCHAR(50), -- In Operation, from Phase 1 to Phase 3
+    description TEXT -- The largest hospital in Nu, and is one of the last bastions of justice in the series.
+                     -- Funded by the goverment and Richoka; they're the golden standard in shifts & medical practices  
+);
