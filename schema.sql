9de6d95556c7146950f65c199b8302578463af34
velayo <velayojohndaniel@gmail.com>
2026-09-21 17:39:45 +0800

    renamed description column to details

--- schema.sql
+++ schema.sql
@@ -22,6 +22,6 @@ CREATE TABLE locations (
     type VARCHAR(50), -- Hospital
     region TEXT, -- Central Nu, near The Medical Recidency
     stat VARCHAR(50), -- In Operation, from Phase 1 to Phase 3
-    description TEXT -- The largest hospital in Nu, and is one of the last bastions of justice in the series.
+    details TEXT -- The largest hospital in Nu, and is one of the last bastions of justice in the series.
                      -- Funded by the goverment and Richoka; they're the golden standard in shifts & medical practices  
 );
